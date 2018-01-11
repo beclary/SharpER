8e13a4c5c2843b3748d921994c98fc4752b63af9
bclary <bclary@PRG-002-PC>
2018-01-11 14:23:23 -0500

    Entered in the fields and properties and method headers for the CONTACTS BLL

---
+++ Contacts-SharpER/Contacts.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Contacts_SharpER
+{
+    public class Contacts
+    {
+        private int contactID;
+        private string contactFirstName;
+        private string contactLastName;
+        private string contactTitle;
+        private string contactDepartment;
+        private string contactAddress;
+        private string contactCity;
+        private string contactState;
+        private string contactZipCode;
+        private string contactPhone;
+        private string contactMobile;
+        private string contactFax;
+        private string contactEmail;
+        private string contactContactedVia;
+        private string contactNotes;
+
+        public int ContactID
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public string ContactFirstName
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public string ContactLastName
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public string ContactTitle
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public string ContactDepartment
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public string ContactAddress
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public string ContactCity
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public string ContactState
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public string ContactZipCode
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public string ContactPhone
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public string ContactMobile
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public string ContactFax
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public string ContactEmail
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public string ContactContactedVia
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public string ContactNotes
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// Make it a static class as well (see p. 237)
+        /// </summary>
+        public int AddContact()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Make it if it updates the row, it is TRUE (see pp. 239-241)
+        /// </summary>
+        public bool UpdateContact()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
