c12e42fa76a521b580f5d0d2b6e4290ca7895d15
bclary <bclary@PRG-002-PC>
2018-01-11 16:01:11 -0500

    enterd in fields and properties

---
+++ Contacts-SharpER/Job.cs
@@ -0,0 +1,107 @@
+﻿// JOB Business Logic Layer (BLL) for SharpER
+// Bruce Clary
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace SharpERBLL    // This is the namespace for all the BLL classes
+{
+    class Job
+    {
+        private int jobID;
+        private string jobPosition;
+        private DateTime jobApplied;
+        private decimal jobPay;
+        private int jobContactID;
+        private int jobCompanyID;
+        private string jobNotes;
+
+        public int JobID
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public string JobPosition
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public DateTime JobApplied
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public decimal JobPay
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public int JobContactID
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public int JobCompanyID
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public string JobNotes
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+    }
+}
