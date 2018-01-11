1a7d6433bee1d34bdebbafab696d0ee349d5080a
bclary <bclary@PRG-002-PC>
2018-01-11 15:11:34 -0500

    Entered in the class diagram for the Company entity

--- Contacts-SharpER/Company.cs
+++ Contacts-SharpER/Company.cs
@@ -8,5 +8,135 @@ namespace SharpERBLL
 {
     class Company
     {
+        private int companyID;
+        private string companyName;
+        private string companyAddress;
+        private string companyCity;
+        private string companyState;
+        private string companyZipCode;
+        private string companyPhone;
+        private string companyFax;
+        private string companyWebsite;
+        private string companyNotes;
+
+        public int CompanyID
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public string CompanyName
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public string CompanyAddress
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public string CompanyCity
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public string CompanyState
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public string CompanyZipCode
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public string CompanyPhone
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public string CompanyFax
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public string CompanyWebsite
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public string CompanyNotes
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
     }
 }
