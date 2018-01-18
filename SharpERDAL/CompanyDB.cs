296fd9aedadc3fa4eafeab98ee1cdb7a45e4c959
bclary <bclary@PRG-002-PC>
2018-01-18 15:41:50 -0500

    put in headers for all classes

--- SharpERDAL/CompanyDB.cs
+++ SharpERDAL/CompanyDB.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// CompanyDB Data Access Layer (DAL) for SharpER
+// Bruce Clary
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
--- SharpERDAL/ContactDB.cs
+++ SharpERDAL/ContactDB.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// ContactDB Data Access Layer (DAL) for SharpER
+// Bruce Clary
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
