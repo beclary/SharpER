226a2b9db01a2f360204db5b4906f21fc363827f
bclary <bclary@PRG-002-PC>
2018-02-02 11:44:19 -0500

    completed the GetSpecificCompanyInfo method

--- SharpERDAL/CompanyDB.cs
+++ SharpERDAL/CompanyDB.cs
@@ -54,7 +54,6 @@ namespace SharpERDAL
                     comRowInfo.CompanyFax = readur.GetString(comCompanyFaxOrd);
                     comRowInfo.CompanyWebsite = readur.GetString(comCompanyWebsiteOrd);
                     comRowInfo.CompanyNotes = readur.GetString(comCompanyNotesOrd);
-
                     companyList.Add(comRowInfo);
                 }
                 readur.Close();
@@ -79,7 +78,61 @@ namespace SharpERDAL
         /// </summary>
         public static Company GetSpecificCompanyInfo(int companyID)
         {
-            throw new System.NotImplementedException();
+            Company specificCompany = new Company();
+            SqlConnection conn = SharpERDB.GetConnection();
+            string selectStmt =
+                "SELECT CompanyID, CompanyName, CompanyAddress, CompanyCity, " +
+                "CompanyState, CompanyZipCode, CompanyPhone, CompanyFax, " +
+                "CompanyWebsite, CompanyNotes " +
+                "FROM Company " +
+                "WHERE CompanyID = @CompanyID";
+            SqlCommand selectCmd = new SqlCommand(selectStmt, conn);
+            selectCmd.Parameters.AddWithValue("@CompanyID", companyID);
+
+            try
+            {
+                conn.Open();
+                SqlDataReader readur = selectCmd.ExecuteReader();
+                int comCompanyIDOrd = readur.GetOrdinal("CompanyID");
+                int comCompanyNameOrd = readur.GetOrdinal("CompanyName");
+                int comCompanyAddressOrd = readur.GetOrdinal("CompanyAddress");
+                int comCompanyCityOrd = readur.GetOrdinal("CompanyCity");
+                int comCompanyStateOrd = readur.GetOrdinal("CompanyState");
+                int comCompanyZipCodeOrd = readur.GetOrdinal("CompanyZipCode");
+                int comCompanyPhoneOrd = readur.GetOrdinal("CompanyPhone");
+                int comCompanyFaxOrd = readur.GetOrdinal("CompanyFax");
+                int comCompanyWebsiteOrd = readur.GetOrdinal("CompanyWebsite");
+                int comCompanyNotesOrd = readur.GetOrdinal("CompanyNotes");
+
+                while (readur.Read())
+                {
+                    Company specificCompanyRowInfo = new Company();
+                    specificCompanyRowInfo.CompanyID = readur.GetInt32(comCompanyIDOrd);
+                    specificCompanyRowInfo.CompanyName = readur.GetString(comCompanyNameOrd);
+                    specificCompanyRowInfo.CompanyAddress = readur.GetString(comCompanyAddressOrd);
+                    specificCompanyRowInfo.CompanyCity = readur.GetString(comCompanyCityOrd);
+                    specificCompanyRowInfo.CompanyState = readur.GetString(comCompanyStateOrd);
+                    specificCompanyRowInfo.CompanyZipCode = readur.GetString(comCompanyZipCodeOrd);
+                    specificCompanyRowInfo.CompanyPhone = readur.GetString(comCompanyPhoneOrd);
+                    specificCompanyRowInfo.CompanyFax = readur.GetString(comCompanyFaxOrd);
+                    specificCompanyRowInfo.CompanyWebsite = readur.GetString(comCompanyWebsiteOrd);
+                    specificCompanyRowInfo.CompanyNotes = readur.GetString(comCompanyNotesOrd);
+                }
+                readur.Close();
+            }
+            catch (SqlException xsept)
+            {
+                throw xsept;
+            }
+            catch (Exception xsept)
+            {
+                throw xsept;
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return specificCompany;
         }
     }
 }
