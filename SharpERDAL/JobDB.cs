33370b4fd5e25753ed7c70990d30bf54822acede
bclary <bclary@PRG-002-PC>
2018-02-15 15:09:38 -0500

    Created the method for Update or Modifying the Job table

--- SharpERDAL/JobDB.cs
+++ SharpERDAL/JobDB.cs
@@ -65,10 +65,82 @@ namespace SharpERDAL
             return jobList;
         }
 
-        /// This will provide the job information given a specific jobID
+        // This will provide the job information given a specific jobID
         public static Job GetSpecificJobInfo(int jobID)
         {
             throw new System.NotImplementedException();
+        }
+
+        // This is the method to update or modify (change) information on a form
+        public static bool UpdateModifyJob (Job oldJob, Job newJob)
+        {
+            SqlConnection conn = SharpERDB.GetConnection();
+            string updateStmt =
+                "UPDATE Job SET " +
+                "JobPosition = @NewJobPosition, " +
+                "JobApplied = @NewJobApplied, " +
+                "JobPay = @NewJobPay, " +
+                "JobContactID = @NewJobContactID, " +
+                "JobCompanyID = @NewJobCompanyID, " +
+                "JobNotes = @NewJobNotes " +
+                "WHERE JobID = @OldJobID " +
+                "AND JobPosition = @OldJobPosition " +
+                "AND JobApplied = @OldJobApplied " +
+                "AND (JobPay = @OldJobPay " +
+                    "OR JobPay IS NULL AND @OldJobPay IS NULL) " +
+                "AND (JobContactID = @OldJobContactID " +
+                    "OR JobContactID IS NULL AND @OldJobContactID IS NULL) " +
+                "AND (JobNotes = @OldJobNotes " +
+                    "OR JobNotes IS NULL AND @OldJobNotes IS NULL)";
+            SqlCommand updateCmd = new SqlCommand(updateStmt, conn);
+
+            // New Job changes
+            updateCmd.Parameters.AddWithValue("@NewJobPosition", newJob.JobPosition);
+            updateCmd.Parameters.AddWithValue("@NewJobApplied", newJob.JobApplied);
+            if (newJob.JobPay == -1)
+                updateCmd.Parameters.AddWithValue("@NewJobPay", DBNull.Value);
+            else
+                updateCmd.Parameters.AddWithValue("@NewJobPay", newJob.JobPay);
+            if (newJob.JobContactID == -1)
+                updateCmd.Parameters.AddWithValue("@NewJobContactID", DBNull.Value);
+            else
+                updateCmd.Parameters.AddWithValue("@NewJobContactID", newJob.JobContactID);
+            if (newJob.JobNotes == "")
+                updateCmd.Parameters.AddWithValue("@NewJobNotes", newJob.JobNotes);
+
+            // Old Job changes
+            updateCmd.Parameters.AddWithValue("@OldJobPosition", oldJob.JobPosition);
+            updateCmd.Parameters.AddWithValue("@OldJobApplied", oldJob.JobApplied);
+            if (oldJob.JobPay == -1)
+                updateCmd.Parameters.AddWithValue("@OldJobPay", DBNull.Value);
+            else
+                updateCmd.Parameters.AddWithValue("@OldJobPay", oldJob.JobPay);
+            if (oldJob.JobCompanyID == -1)
+                updateCmd.Parameters.AddWithValue("@OldJobContactID", DBNull.Value);
+            else
+                updateCmd.Parameters.AddWithValue("@OldJobContactID", oldJob.JobContactID);
+            if (oldJob.JobNotes == "")
+                updateCmd.Parameters.AddWithValue("@OldJobNotes", DBNull.Value);
+            else
+                updateCmd.Parameters.AddWithValue("@OldJobNotes", oldJob.JobNotes);
+
+            try
+            {
+                conn.Open();
+                int count = updateCmd.ExecuteNonQuery();
+                if (count > 0)
+                    return true;
+                else
+                    return false;
+            }
+            catch (SqlException xsept)
+            {
+                throw xsept;
+            }
+            finally
+            {
+                conn.Close();
+            }
         }
     }
 }
