ba330c604656fc94e105414d590d3c31bddc94bb
bclary <bclary@PRG-002-PC>
2018-02-15 11:50:53 -0500

    changed some comments in Job .cd file and printed it for notebook

--- SharpERDAL/JobDB.cs
+++ SharpERDAL/JobDB.cs
@@ -13,9 +13,7 @@ namespace SharpERDAL
 {
     public static class JobDB
     {
-        /// <summary>
-        /// This will provide a listing of all Jobs that are available in the database
-        /// </summary>
+        // This will provide a listing of all Jobs that are available in the database
         public static List<Job> GetAllJobs()
         {
             List<Job> jobList = new List<Job>();
@@ -67,9 +65,7 @@ namespace SharpERDAL
             return jobList;
         }
 
-        /// <summary>
         /// This will provide the job information given a specific jobID
-        /// </summary>
         public static Job GetSpecificJobInfo(int jobID)
         {
             throw new System.NotImplementedException();
